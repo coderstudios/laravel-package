2f83cdc12b0ff7a60227373d3d9623d46e74cdea
David Wright <dave@coderstudios.com>
2017-04-17 22:20:11 +0100

    Composer update

--- src/CoderStudios/LP/Composers/MasterComposer.php
+++ src/CoderStudios/LP/Composers/MasterComposer.php
@@ -2,18 +2,12 @@
 
 namespace CoderStudios\LP\Composers;
 
-use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
 class MasterComposer {
 
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
 	public function compose(View $view)
 	{
-        $view->with('title','LP Package');
+		$view->with('title','LP Package');
 	}
 }
